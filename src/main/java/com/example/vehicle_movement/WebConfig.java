da02df2b7d5d00290bc60912c71a525d14d80815
mahesh-gaikwad-7 <maheshgaikwad.sae.comp@gmail.com>
2025-01-28 13:45:01 +0530

    first commit

---
+++ src/main/java/com/example/vehicle_movement/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.vehicle_movement;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://mahesh-gaikwad-7.github.io/vehicle-tracking-frontend/") // Your updated GitHub Pages URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
